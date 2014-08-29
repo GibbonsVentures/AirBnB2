76ac13a2dc657a31f77d4033c1ce77a640a551ee
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-08-29 16:13:51 -0400

    amazon S3

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,9 +1,10 @@
 class Picture < ActiveRecord::Base
 	belongs_to :place
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	#validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-
+	
+	validates :image, presence: true
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
 
 
