3c16f4f6470087c2f1fdfc87294b2b95802cf607
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-08-29 16:51:37 -0400

    picture

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,15 +5,9 @@ class Picture < ActiveRecord::Base
 	#validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 	validates :image, presence: true
+	has_attached_file :image, :styles => { :medium => "300x300>"}
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
-	 has_attached_file :image, 
-            :style => { :medium => "300x300>", :thumb => "100x100>" },
-            :storage => :s3,
-            :bucket  => ENV['AWS_BUCKET'],
-            :s3_credentials => {
-                    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-                },
+	 
 
 
 end
